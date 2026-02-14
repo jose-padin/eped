c2b07ca09e60ab9d9f58505aa5e31243972ec083
Jose Padín <jmendez@heliovolta.com>
2026-02-14 13:30:21 +0100

    refactor: update ListIF methods

--- src/tema01/ListIPIF.java
+++ src/tema01/ListIPIF.java
@@ -4,9 +4,32 @@ import tema01.SequenceIF;
 
 public interface ListIPIF<E> extends SequenceIF<E> {
 
+	/*
+	 * Return the next element in the list.
+	 * @Pre: there are elements in the list.
+	 * @return: the next element.
+	 */
 	public E get();
 
-	public E set(E e);
+	/*
+	 * Modify the current position of the iterator with the value of e.
+	 * @param e The element to set.
+	 */
+	public void set(E e);
 
+	/*
+	 * Remove the element in the position of the iterator.
+	 */
 	public void remove();
+
+	/*
+	 * Move the iterator one position backwards.
+	 */
+	public void moveBackwards();
+
+	/*
+	 * Move the iterator one position forwards.
+	 */
+	public void moveForwards();
+
 }
