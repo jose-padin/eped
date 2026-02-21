f814b76ec0cfabc6e29fd3f8347cb50eb0fb932e
Jose Padín <jmendez@heliovolta.com>
2026-02-21 18:05:57 +0100

    refactor: add removeHead method & update comments

--- src/tema01/ListHTIF.java
+++ src/tema01/ListHTIF.java
@@ -9,17 +9,19 @@ import tema01.SequenceIF;
  * las operaciones, además, deberán devolver un valor (es decir, su tipo de salida
  * no puede ser void). ¿Dónde situaría este interfaz en el mapa de TAD de la
  * asignatura?
- *  Respuesta: estaría situado debajo de Sequence que, a su vez, estaría situado debajo de Collection.
+ * Estaría situado debajo de Sequence que, a su vez, estaría situado debajo de Collection.
  */
+
 public interface ListHTIF<E> extends SequenceIF<E> {
 
 	/*
 	 * Insert an element in the list replacing the one existing in the passed position.
 	 * @Pre: !isEmpty().
 	 * @Pos: size == size() + 1.
+	 * @param e The element to be set as the head.
 	 * return: true if the element was inserted; false if not.
 	 */
-	public boolean setHead(E element);
+	public boolean setHead(E e);
 
 	/*
 	 * Get the head of the list.
@@ -30,15 +32,23 @@ public interface ListHTIF<E> extends SequenceIF<E> {
 
 	/*
 	 * Set the tail of a list.
+	 * @param e The list to insert as a tail.
 	 * return true if the insertion was correct or false otherwise.
 	 */
 	public boolean setTail(ListHTIF e);
 
 	/*
-	 * Return the tail of a list;
-	 * @Pre: !isEmpty()
+	 * Return the tail of a list.
+	 * @Pre: !isEmpty().
 	 * return the tail list.
 	 */
 	public ListHTIF<E> getTail();
+
+	/*
+	 * Remove the head.
+	 * @Pre: !isEmpty().
+	 * return true if the head has been removed; false otherwise.
+	 */
+	public boolean removeHead();
 
 }
