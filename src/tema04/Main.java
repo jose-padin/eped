25fe5c4e3df7e8f1ff48d0607afe515e8a2599c4
Jose Padín <jmendez@heliovolta.com>
2026-04-26 11:59:36 +0200

    Test in Main

--- src/tema04/Main.java
+++ src/tema04/Main.java
@@ -1,6 +1,8 @@
 package tema04;
 
 import tema01.IteratorIF;
+import tema01.ListIF;
+import tema04.List;
 
 //Realizar una implementación de ListIPIF<E> (Listas con Punto de Interés)
 //utilizando dos pilas como Estructura de Datos de soporte. Calcular el coste de
@@ -8,40 +10,22 @@ import tema01.IteratorIF;
 public class Main {
 
 	public static void main(String[] args) {
-		ListIP<Integer> list = new ListIP<Integer>();
-		// The pointer is never moved (position == 1), so they must be inserted at the beginning.
-		list.insert(1);
-		list.insert(2);
-		list.insert(3);
-		list.insert(4);
+		List<Integer> list = new List<Integer>();
+		list.insert(1, 1);
+		list.insert(2, 2);
+		list.insert(3, 3);
+		list.insert(4, 4);
 
-		IteratorIF<Integer> it = list.iterator();
-
+		IteratorIF it = list.iterator();
 		while (it.hasNext()) {
 			System.out.println(it.getNext());
 		}
-
-		// Removing
-		list.remove();
-		list.remove();
-		it.reset();
-		System.out.println("-----");
-		while (it.hasNext()) {
-			// It should print 2, 1
-			System.out.println(it.getNext());
-		}
-
-		System.out.printf("pointer: %d\n", list.getPointer());
-		list.moveForwards();
-		System.out.printf("pointer: %d\n", list.getPointer());
-		System.out.println(list.get());
-		System.out.printf("pointer: %d\n", list.getPointer());
-		list.remove();
-		it.reset();
 		System.out.println("---");
-		while (it.hasNext()) {
-			System.out.println(it.getNext());
+
+		ListIF<Integer> inverted = list.invierte(list);
+		IteratorIF it2 = inverted.iterator();
+		while (it2.hasNext()) {
+			System.out.println(it2.getNext());
 		}
 	}
 }
-
